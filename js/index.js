903865c482a1ea931a55259114b8e8599f829ccc
Leandro Moscheni <leandro.moscheni@despegar.com>
2021-01-10 00:34:17 -0300

    remove js to remove class hover

--- js/index.js
+++ js/index.js
@@ -27,6 +27,4 @@ window.onscroll = function () {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  // const mybutton = document.getElementById("toTopButton");
-  // mybutton.classList.remove("hover");
 }
