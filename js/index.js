ec91b053405b06172f37120cd008b27a55619bfe
Leandro Moscheni <leandro.moscheni@despegar.com>
2021-01-09 23:50:36 -0300

    Adding basic scroll to top button

--- js/index.js
+++ js/index.js
@@ -12,3 +12,20 @@ function validateForm(event) {
   }
 }
 
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  const mybutton = document.getElementById("toTopButton");
+  console.log(mybutton);
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
