c5aaf775d3fd789a34254837c7d95ce2b4b13093
Leandro Moscheni <leandro.moscheni@despegar.com>
2021-03-01 22:25:34 -0300

    Optimize files, improve SEO and improve accesibilty

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-const EMAIL_FORMAT = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-function validateForm(event) {
-  const emailInput = document.forms["contact-form"]["_replyto"];
-  const isValidEmail = EMAIL_FORMAT.test(String(emailInput.value).toLowerCase());
-
-  if (isValidEmail) {
-    emailInput.classList = emailInput.classList.filter(e => e !=='invalid-input');
-  } else {
-    emailInput.classList.add("invalid-input")
-    return false;
-  }
-}
-
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   const mybutton = document.getElementById("toTopButton");
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
