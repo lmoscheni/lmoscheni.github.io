e3360a02567bd0283367ee201b1e6bec111ed7ea
Leandro Moscheni <leandro.moscheni@despegar.com>
2021-05-05 13:24:00 -0300

    Adding portfolio section and menu for mobile

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 window.onscroll = function () {
   const mybutton = document.getElementById("toTopButton");
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
     mybutton.style.display = "block";
   } else {
-      mybutton.style.display = "none";
+    mybutton.style.display = "none";
   }
 };
 
@@ -11,4 +14,43 @@ window.onscroll = function () {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+function listenMenuButton() {
+  const menuBtn = document.getElementById("menu-btn");
+  const menu = document.getElementById("menu");
+
+  let menuOpen = false;
+  menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menu.classList.add("open");
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove("open");
+      menu.classList.remove("open");
+      menuOpen = false;
+    }
+  });
+
+  menu.childNodes
+    .item(1)
+    .childNodes.item(1)
+    .childNodes.forEach((e) => {
+      if (e.tagName === "LI") {
+        e.addEventListener("click", () => {
+          if (!menuOpen) {
+            menuBtn.classList.add("open");
+            menu.classList.add("open");
+            menuOpen = true;
+          } else {
+            menuBtn.classList.remove("open");
+            menu.classList.remove("open");
+            menuOpen = false;
+          }
+        });
+      }
+    });
+
 }
